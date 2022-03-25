5b72146dadfaf3d1719483a2d860e6580b54e0f0
SHUNSUKE-OHTSUKA <s.ohtsuka0813@gmail.com>
2022-03-25 21:36:29 +0900

    'init'

---
+++ fastapi/app/model.py
@@ -0,0 +1,29 @@
+# https://qiita.com/satto_sann/items/4fbc1a4e2b33fa2237d2
+from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel
+from db import Base
+from db import ENGINE
+
+
+# userテーブルのモデルUserTableを定義
+class UserTable(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(30), nullable=False)
+    age = Column(Integer)
+
+
+# POSTやPUTのとき受け取るRequest Bodyのモデルを定義
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+
+def main():
+    # テーブルが存在しなければ、テーブルを作成
+    Base.metadata.create_all(bind=ENGINE)
+
+
+if __name__ == "__main__":
+    main()
